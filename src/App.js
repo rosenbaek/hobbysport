2078ec2da9466dca18101eb1e4b91b6684413472
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2022-04-26 12:46:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import ProtectedScreen from "./screens/ProtectedScreen";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDvzi-Yx-rOVV1A5hiwl8f1Xa_qFy-sCR0",
+	authDomain: "hobbysport-91224.firebaseapp.com",
+	projectId: "hobbysport-91224",
+	storageBucket: "hobbysport-91224.appspot.com",
+	messagingSenderId: "234830154797",
+	appId: "1:234830154797:web:28c27ca8a788502ea33ca4",
+	measurementId: "G-DNMPRXQ6HT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function App() {
+	return (
+		<>
+			<h1>Welcome to React Router!</h1>
+			<Routes>
+				<Route path="/" element={<ProtectedScreen />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
---
+++ src/screens/ProtectedScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProtectedScreen = () => {
+	return (
+		<>
+			<h2>(Protected)</h2>
+		</>
+	);
+};
+export default ProtectedScreen;
