7a18d2453e49173f0754175ce47cbbb77751ac8b
rosenbaek <59441104+rosenbaek@users.noreply.github.com>
2022-04-27 11:04:52 +0200

    React Router protected route implemented

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-import ProtectedScreen from './screens/ProtectedScreen';
-import HomePage from './screens/HomePage';
-import Navbar from './components/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import ProtectedScreen from "./screens/ProtectedScreen";
+import HomePage from "./screens/HomePage";
+import Navbar from "./components/Navbar";
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyDvzi-Yx-rOVV1A5hiwl8f1Xa_qFy-sCR0',
-  authDomain: 'hobbysport-91224.firebaseapp.com',
-  projectId: 'hobbysport-91224',
-  storageBucket: 'hobbysport-91224.appspot.com',
-  messagingSenderId: '234830154797',
-  appId: '1:234830154797:web:28c27ca8a788502ea33ca4',
-  measurementId: 'G-DNMPRXQ6HT',
+	apiKey: "AIzaSyDvzi-Yx-rOVV1A5hiwl8f1Xa_qFy-sCR0",
+	authDomain: "hobbysport-91224.firebaseapp.com",
+	projectId: "hobbysport-91224",
+	storageBucket: "hobbysport-91224.appspot.com",
+	messagingSenderId: "234830154797",
+	appId: "1:234830154797:web:28c27ca8a788502ea33ca4",
+	measurementId: "G-DNMPRXQ6HT",
 });
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
-  return (
-    <>
-      <Navbar auth={auth} />
-      <div className="container mx-auto">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/protected" element={<ProtectedScreen />} />
-        </Routes>
-      </div>
-    </>
-  );
+	const [user] = useAuthState(auth);
+	return (
+		<>
+			<Navbar auth={auth} />
+			<div className="container mx-auto">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route
+						path="/protected"
+						element={
+							<ProtectedRoute user={user}>
+								<ProtectedScreen />
+							</ProtectedRoute>
+						}
+					/>
+				</Routes>
+			</div>
+		</>
+	);
 }
 
 export default App;
